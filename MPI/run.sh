1370112ac45299129265cb11b2ffdf90d018a0de
Unknown <yfw215@nyu.edu>
2021-03-22 00:30:48 +0800

    Debugged

--- MPI/run.sh
+++ MPI/run.sh
@@ -1,5 +1,5 @@
-for MINSUP in 1 2 3 .. 50
+for MINSUP in 1 6 11 16 21 26 31 36 41 46
 do 
-	time mpiexec --oversubscribe -np 9 -H localhost python3 run.py --database retail --minsup $MINSUP >> retail_out.txt
+	time -p mpiexec --oversubscribe -np 9 -H localhost python3 run.py --database retail --minsup $MINSUP >> retail_out.txt
 done
 
